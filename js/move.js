68f84ad99bfaa40fabb7072c05d8790f408ba254
PADED KUMWONG <PADED@ecartstudio.com>
2017-08-11 16:31:20 +0700

    add style for

--- js/move.js
+++ js/move.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
     $(".bottom-panel").click(function(){
+         
         if($(this).hasClass('expanded')){
-            $(this).removeClass('expanded').stop().animate({"bottom":"-90%"},200);
+             $(".search-box").animate({"top":"10px"},200);
+            $(this).removeClass('expanded').stop().animate({"bottom":"-92%"},200,function(){
+                $("#down-arrow").remove();
+                 var txt2 = $("<i></i>",{id:"up-arrow","class": "material-icons md-36"}).text("keyboard_arrow_up");
+                            $("#more-btn-icon").append(txt2);
+                 $(this).css({"border-radius":"16px 16px 0px 0px"},200);
+            });
         }else{
-            $(this).addClass('expanded').stop().animate({"bottom":"0"},200);
+            $(".search-box").animate({"top":"-200px"},200);
+            $(this).addClass('expanded').stop().animate({"bottom":"0"},200,function(){
+                    $(this).css({"border-radius":"0px 0px 0px 0px"},200)
+            });
+            $("#up-arrow").remove();
+            var txt2 = $("<i></i>",{id:"down-arrow","class": "material-icons md-36"}).text("keyboard_arrow_down");
+            $("#more-btn-icon").append(txt2);
         }
     });
 });
